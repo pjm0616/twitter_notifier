4cbbd92016ade7860b5d09bc02b13a0b9578719a
Park, Jeongmin <pjm0616@gmail.com>
2012-07-24 16:37:29 +0900

    add 'tweet' command

--- twt_us_notifier.py
+++ twt_us_notifier.py
@@ -7,6 +7,8 @@ import urllib2
 import re
 import json
 import os
+import sys
+import getopt
 from BeautifulSoup import BeautifulStoneSoup
 
 from getpass import getpass
@@ -17,8 +19,8 @@ import tweepy
 
 pynotify.init('twitter_notifier')
 
-g_config_filename = u'./config.txt'
-g_config = None
+g_twitter = None
+
 
 def decodehtmlentities(data):
 	return unicode(BeautifulStoneSoup(data, convertEntities=BeautifulStoneSoup.HTML_ENTITIES))
@@ -35,7 +37,7 @@ def download(url, filename):
 	open(filename, u'w').write(data)
 
 def get_image(url):
-	if g_config.get('get_original_img', False):
+	if g_twitter.config.get('get_original_img', False):
 		url = re.sub('_normal(?=\.)', '', url)
 
 	hashed = hashlib.sha1(url).hexdigest()
@@ -49,16 +51,15 @@ def get_image(url):
 	
 	return filename
 
-tweetlogfile = open(u'./tweets.txt', u'a')
 
 class StreamWatcherListener(tweepy.StreamListener):
 	status_wrapper = TextWrapper(width=60, initial_indent=u'    ', subsequent_indent=u'    ')
 	
 	def on_data(self, data):
 		super(StreamWatcherListener, self).on_data(data)
-		tweetlogfile.write(data)
-		tweetlogfile.write(u'\n')
-		tweetlogfile.flush()
+		g_twitter.streamlogfile.write(data)
+		g_twitter.streamlogfile.write(u'\n')
+		g_twitter.streamlogfile.flush()
 
 	def on_status(self, status):
 		try:
@@ -69,7 +70,8 @@ class StreamWatcherListener(tweepy.StreamListener):
 			# sometimes status update lacks `author' key
 			return
 
-		print '\033[1m' + self.status_wrapper.fill(status.text) + '\033[0m'
+		print u'tweet %s' % status.id
+		print u'\033[1m' + self.status_wrapper.fill(status.text) + '\033[0m'
 		print u'%s\n========================================\n' % etcinfo_term
 
 		statustext = decodehtmlentities(status.text)
@@ -83,48 +85,116 @@ class StreamWatcherListener(tweepy.StreamListener):
 	def on_timeout(self):
 		print u'timeout'
 
-def loadcfg():
-	global g_config
-	try:
-		g_config = json.load(open(g_config_filename))
-	except (IOError, ValueError):
-		key = raw_input(u'consumer_key: ')
-		secret = raw_input(u'consumer_secret: ')
-		g_config = {'consumer_key': key, 'consumer_secret': secret}
-def savecfg():
-	global g_config
-	json.dump(g_config, open(g_config_filename, u'w'))
+class Twitter(object):
+	def __init__(self):
+		self.cfgfile= None
+		self.config = {}
+		self.streamlogfile = open(u'./tweets.txt', u'a')
+
+	def _loadcfg(self, filename):
+		try:
+			self.config = json.load(open(filename))
+		except (IOError, ValueError):
+			key = raw_input(u'consumer_key: ')
+			secret = raw_input(u'consumer_secret: ')
+			self.config = {'consumer_key': key, 'consumer_secret': secret}
+	def init(self, cfgfile):
+		self.cfgfile = cfgfile
+		self._loadcfg(cfgfile)
+		self.authorize()
+	def savecfg(self, filename):
+		json.dump(self.config, open(filename, u'w'))
+
+	def authorize(self):
+		auth = tweepy.OAuthHandler(self.config[u'consumer_key'], self.config[u'consumer_secret'])
+		if u'access_token_key' not in self.config:
+			try:
+				redirect_url = auth.get_authorization_url()
+			except tweepy.TweepError as e:
+				print u'Error! Failed to get request token.'
+				print(e)
+			else:
+				print(u'Authorization URL: %s' % redirect_url)
+				pincode = raw_input(u'enter PIN code: ')
+				auth.get_access_token(pincode)
+				print(u'Key: %s' % auth.access_token.key)
+				print(u'Secret: %s' % auth.access_token.secret)
+
+				self.config[u'access_token_key'] = auth.access_token.key
+				self.config[u'access_token_secret'] = auth.access_token.secret
+				# FIXME
+				self.savecfg(self.cfgfile)
+
+				print(u'OK. Now restart the program.')
+
+			return
+		else:
+			auth.set_access_token(self.config[u'access_token_key'], self.config[u'access_token_secret'])
+			self.auth = auth
+			self.api = tweepy.API(auth)
+
+	def main_userstream(self, argv):
+		opts, args = getopt.gnu_getopt(argv, u'h')
+		for o, a in opts:
+			if o == u'-h':
+				print(u'%s: Start user stream notification service' % argv[0])
+				print(u'Usage: %s' % argv[0])
+				return
+		stream = tweepy.Stream(self.auth, StreamWatcherListener(), timeout=None, secure=True)
+		stream.userstream()
+
+	def main_tweet(self, argv):
+		opts, args = getopt.gnu_getopt(argv, u'hr:')
+		reply_to = None
+		for o, a in opts:
+			if o == u'-h':
+				print(u'%s: update status' % argv[0])
+				print(u'Usage: %s [<status text>]' % argv[0])
+				return
+			elif o == u'-r':
+				reply_to = a
+		text = ' '.join(args[1:])
+		if text == '':
+			text = raw_input()
+			if text == '':
+				return False
+		res = self.api.update_status(text, in_reply_to_status_id=reply_to)
+		if not res:
+			return False
+
+	def main_main(self, argv):
+		print(u'Usage: %s <program_name> [-h] args...' % argv[0])
+		print(u'Programs:')
+		print(u'\tuserstream')
+		print(u'\ttweet')
 
 def main():
-	global g_config
-	loadcfg()
+	global g_twitter
+	g_twitter = Twitter()
 
-	auth = tweepy.OAuthHandler(g_config[u'consumer_key'], g_config[u'consumer_secret'])
-	if u'access_token_key' not in g_config:
-		try:
-			redirect_url = auth.get_authorization_url()
-		except tweepy.TweepError as e:
-			print u'Error! Failed to get request token.'
-			print(e)
+	programs = {
+		u'userstream': g_twitter.main_userstream,
+		u'tweet': g_twitter.main_tweet,
+	}
+	argv = sys.argv
+	try:
+		program = programs[argv[0]]
+	except KeyError:
+		if len(argv) >=2:
+			try:
+				program = programs[argv[1]]
+				argv = argv[1:]
+			except KeyError:
+				program = g_twitter.main_main
 		else:
-			print(u'Authorization URL: %s' % redirect_url)
-			pincode = raw_input(u'enter PIN code: ')
-			auth.get_access_token(pincode)
-			print(u'Key: %s' % auth.access_token.key)
-			print(u'Secret: %s' % auth.access_token.secret)
+			program = g_twitter.main_main
 
-			g_config[u'access_token_key'] = auth.access_token.key
-			g_config[u'access_token_secret'] = auth.access_token.secret
-			savecfg()
-
-			print(u'OK. Now restart the program.')
-
-		return
+	g_twitter.init(u'./config.txt')
+	ret = program(argv)
+	if ret != False:
+		sys.exit(0)
 	else:
-		auth.set_access_token(g_config[u'access_token_key'], g_config[u'access_token_secret'])
-
-	stream = tweepy.Stream(auth, StreamWatcherListener(), timeout=None, secure=True)
-	stream.userstream()
+		sys.exit(1)
 
 if __name__ == u'__main__':
 	try:
